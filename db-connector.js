56858966c8c6dc50d09395afe5063ab7c846b4ae
Saif Sulaiman <saif.silver@gmail.com>
2021-08-25 13:39:16 +0530

    DB Connection string changed to live

--- db-connector.js
+++ db-connector.js
@@ -1,16 +1,12 @@
 const fastifyPlugin = require('fastify-plugin')
 
 async function dbConnector(fastify, options) {
-    //fastify.register(require('fastify-postgres'), {
-    //     connectionString: "postgres://dfotppexlqphxr:7dc4a1ec44559bab4e401e2c36ab3e384baa1bfd76a6cf95ee008551c820fa85@ec2-54-156-60-12.compute-1.amazonaws.com:5432/ddkbf5uk00camd",
-    //     ssl: {
-    //         rejectUnauthorized: false
-    //     }
-    // });
-
     fastify.register(require('fastify-postgres'), {
-        connectionString: "postgres://test:test@localhost:5432/indian_banks"
-      });
+        connectionString: "postgres://dfotppexlqphxr:7dc4a1ec44559bab4e401e2c36ab3e384baa1bfd76a6cf95ee008551c820fa85@ec2-54-156-60-12.compute-1.amazonaws.com:5432/ddkbf5uk00camd",
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
 }
 
 // Wrapping a plugin function with fastify-plugin exposes the decorators    
